a9bb073430ce67a02afaf34adffa3d1a4a01a880
Zachary <zepm0122.zn@gmail.com>
2024-06-07 13:00:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names= [
+    "Guadalupe",
+    "Ollie",
+    "Aki",
+]
+const thankYou = []
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        //console.log(`Thank you ${names[i]} for the wonderful suprise gift`)
+        //debugger;
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    console.log(thankYou)
+    return thankYou
+}
+function countDown() {
+    for (let i = 9; i >= 0; i--) {
+      console.log(i);
+    }
+    console.log(countDown)
+  }
+
+
